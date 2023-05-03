77d4964e94acac5e273bfbd597776201d0a5d064
Paulo <pauloxti@gmail.com>
2023-05-03 03:24:50 +0200

    wallet connection

--- meta-portal/src/layout/Header.js
+++ meta-portal/src/layout/Header.js
@@ -3,11 +3,65 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { navigationToggle, walletToggle } from "../redux/actions/siteSettings";
 import { stickyNav } from "../utilits";
+import { useWeb3React } from "@web3-react/core";
+import { injected, swithNetwork } from "../blockchain/metamaskConnector";
+import ChainParams from "../blockchain/chainParams";
 
 const Header = ({ walletToggle, navigationToggle }) => {
   useEffect(() => {
     stickyNav();
   }, []);
+
+  const selectedChainId = 421613;
+  const { active, account, library, activate, deactivate, chainId } =
+    useWeb3React();
+
+  useEffect(() => {
+    swithNetwork(ChainParams[0]);
+
+    /*
+    if (active && chainId !== selectedChainId) {
+      swithNetwork(ChainParams[0]);
+    }
+    */
+  }, [chainId]);
+
+  useEffect(() => {
+    const isWalletConnected = localStorage.getItem("isWalletConnected");
+    const connector = localStorage.getItem("connector");
+    if (isWalletConnected === "true" && connector === "injected") {
+      activate(injected);
+    }
+  }, [active]);
+
+  async function connectMetamaks() {
+    try {
+      await activate(injected, undefined, true);
+      localStorage.setItem("connector", "injected");
+      localStorage.setItem("isWalletConnected", "true");
+    } catch (ex) {
+      console.log(ex.code);
+    }
+  }
+
+  function getWalletAbreviation(
+    walletAddress
+  ) {
+    if (walletAddress !== null && walletAddress !== undefined) {
+      return walletAddress.slice(0, 6) + "..." + walletAddress.slice(-4);
+    }
+    return "";
+  }
+
+  async function disconnectMetamaks() {
+    try {
+      deactivate();
+      localStorage.setItem("isWalletConnected", "false");
+      localStorage.removeItem("connector");
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
 
   return (
     <header id="header">
@@ -42,16 +96,16 @@ const Header = ({ walletToggle, navigationToggle }) => {
             </ul>
           </div>
           <div className="wallet">
-            <a
-              href="#"
-              onClick={(e) => {
-                e.preventDefault();
-                walletToggle(true);
-              }}
-              className="metaportal_fn_button wallet_opener"
-            >
-              <span>Connect To Wallet</span>
-            </a>
+            {active ? (
+              <a className="metaportal_fn_button wallet_opener" onClick={disconnectMetamaks}>
+                <span>{getWalletAbreviation(account)}</span>
+              </a>
+            ) : (
+
+              <a className="metaportal_fn_button wallet_opener" onClick={connectMetamaks}>
+                <span>Connect Wallet</span>
+              </a>
+            )}
           </div>
         </div>
       </div>
